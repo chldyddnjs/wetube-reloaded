7643339d4d11c46c904cf1aea2bc1996ed2d9e57
chldyddnjns <dyddnjsdl321@naver.com>
2021-10-04 14:54:25 +0900

    first commit

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { getEdit,postEdit,remove,logout,see, startGithunLogin, finishGithubLogin } from "../controllers/userController";
+import { getEdit,postEdit,remove,logout,see, startGithunLogin, finishGithubLogin, getChangePassword, postChangePassword } from "../controllers/userController";
 import { protectorMiddleware, publicOnlyMiddleware } from "../middlewares";
 const userRouter = express.Router();
 userRouter.get("/logout",protectorMiddleware,logout);
 userRouter.route("/edit").all(protectorMiddleware).get(getEdit).post(postEdit);
+userRouter.route("/change-password").all(protectorMiddleware).get(getChangePassword).post(postChangePassword);
 userRouter.get("/delete",remove);
 userRouter.get("/github/start",publicOnlyMiddleware,startGithunLogin);
 userRouter.get("/github/finish",publicOnlyMiddleware,finishGithubLogin);
